5c2c0f3ba4d9a7094e6523613a26b8ee14027a48
arjnramesh281 <arjunramesh281@gmail.com>
2024-09-18 11:31:46 +0530

    javascript

---
+++ script.js
@@ -0,0 +1,83 @@
+// alert(" A error in this page")
+// a=10
+
+
+// var a
+// a=1000
+
+// let a=10
+// a="helloo!!!"
+
+// const a=100 
+// alert(a)
+
+// _____________________datatypes in javascript________________________________
+
+// let b=1921
+// alert(typeof b)
+
+// let a="hello!"
+// alert(typeof a)
+
+// let a=false
+// alert(typeof a)
+
+// let a
+// alert(typeof a)
+
+// let a=null
+// alert(typeof a) 
+
+// let a=[1,2,3,37]
+// alert(typeof a) 
+
+// let a={a:"12"}
+// alert(typeof a) 
+
+// ______________________operators  in javascript_______________________________
+
+// _____arithemetic operator____
+
+// let a=10
+// let b=5
+
+// alert(a+b)
+// alert(a-b)
+// alert(a*b)
+// alert(a/b)
+// alert(a%b)
+// a++
+// a--
+// alert(a)
+
+// _____assignment operator_____
+
+// let a=10
+// a+=10
+// a-=10
+// a*=10
+// a/=10
+// a%=10
+// alert(a)
+
+// _____comparison operator______
+
+// let a=10
+// let b=10
+// alert(a===b)
+// alert(a!==b)
+// alert(a==b)
+// alert(a!=b)
+// alert(a>=b)
+// alert(a<=b)
+// alert(a>b)
+// alert(a<b)
+
+// ____logical operator______
+
+// let a=10
+// let b=10
+
+// alert(a==b && a>b)
+// alert(a==b || a>b)
+// alert(!a==b)
