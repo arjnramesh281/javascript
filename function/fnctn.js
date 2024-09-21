3e6f5f6623e1d99731329f74bbc278a71b4f0f88
arjnramesh281 <arjunramesh281@gmail.com>
2024-09-21 11:28:03 +0530

    javascript

---
+++ function/fnctn.js
@@ -0,0 +1,24 @@
+
+// ______________________normal function_________
+
+// function sample(a,b){
+//     console.log("normal function",a,b);
+    
+// }
+// sample(10,20)
+
+// ______________________arrow function__________
+
+a=(a,b)=>{
+    console.log(" arrow function",a,b);
+    
+}
+a(122,229)
+
+// ______________________nameless function_______
+
+b=function(x,y){
+    console.log("nameless function",x,y);
+    
+}
+b(12,87)
---
+++ str_methods/stringmethds.js
@@ -0,0 +1,24 @@
+a="some string methods in js"
+b="THE BEGINING AFTER THE END"
+c="                 this is third line code           "
+console.log(a[0]);
+console.log(a.length);
+console.log(a.toUpperCase());
+console.log(b.toLowerCase());
+console.log(c.trim());
+console.log(c.trimStart());
+console.log(c.trimEnd());
+console.log(a.charAt(1));
+console.log(b.charCodeAt(13));
+
+x="Hello Its september 2024"
+
+
+console.log(x.match("Hello?"));
+console.log(x.match("[a-z0-9]"));
+console.log(x.match("[a-z][0-9]"));
+console.log(x.search("[a-z]"));
+console.log(x.match("I.+"));
+console.log(x.match("sep.?"));
+console.log(x.match("sep.*"));
+
