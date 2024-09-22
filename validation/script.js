325834bda4988e5ca1128ca619c6d5b7774b6e64
arjnramesh281 <arjunramesh281@gmail.com>
2024-09-22 14:55:03 +0530

    javascript

--- validation/script.js
+++ validation/script.js
@@ -1,3 +1,24 @@
 function valid(){
-    document.getElementById
+    document.getElementById("num_error").innerHTML=""
+    document.getElementById("pin_error").innerHTML=""
+    document.getElementById("email_error").innerHTML=""
+
+    name=document.getElementById("name").value
+    phno=document.getElementById("phno").value
+    email=document.getElementById("email").value
+    pin=document.getElementById("pin").value
+    console.log(name,phno,email,pin);
+    if(phno.length!=10){
+        document.getElementById("num_error").innerHTML="invalid number"
+    }
+    
+    if(pin.length!=6){
+        document.getElementById("pin_error").innerHTML="invalid pin"
+    }
+    if(email.match("@gmail.com")){
+    }
+    else{
+        document.getElementById("email_error").innerHTML="invalid email"
+    }
+
 }
